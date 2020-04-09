12ee8012141c25bcdffc307917b4b6bbe4287694
andy-thomason <andy@andythomason.com>
2020-04-09 15:03:12 +0100

    Still attempting to resolve docs.rs issues.

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-link-lib=R");
 
     println!("cargo:rerun-if-changed=wrapper.h");
