b741f97d77d1efca03cbae763e8bcbc6c48419a7
Hiroaki Yutani <yutani.ini@gmail.com>
2021-08-24 21:21:49 +0900

    Remove unnecessary format()! (#77)
    * Remove unnecessary format()!
    
    * Minimize the change

--- build.rs
+++ build.rs
@@ -348,10 +348,8 @@ fn retrieve_prebuild_bindings(version_info: &RVersionInfo) {
         from = bindings_path.join(bindings_file_novers);
         if !from.exists() {
             panic!(
-                format!(
-                    "Cannot find libR-sys bindings file for R {}.{}.{}{} on {} in {}. Consider compiling with --features use-bindgen.",
-                    version_info.major, version_info.minor, version_info.patch, version_info.devel, target_os, bindings_path.display()
-                )
+                "Cannot find libR-sys bindings file for R {}.{}.{}{} on {} in {}. Consider compiling with --features use-bindgen.",
+                version_info.major, version_info.minor, version_info.patch, version_info.devel, target_os, bindings_path.display()
             )
         } else {
             println!(
