85377ac8a7bbf0e3e6d4a3c9a81fdefa397ab961
Hiroaki Yutani <yutani.ini@gmail.com>
2023-03-10 10:28:54 +0900

    Add some comment

--- wrapper.h
+++ wrapper.h
@@ -10,6 +10,12 @@ typedef ptrdiff_t R_xlen_t_rust;
 // #define HAVE_UINTPTR_T
 #define CSTACK_DEFNS
 
+// From r83513 (R 4.3), R defines the `NORET` macro differently depending on the
+// C/C++ standard the compiler uses. It matters when the header is used in C/C++
+// libraries, but all we want to do here is to make bindgen interpret `NOREP` to
+// `!`. However, for some reason, bindgen doesn't handle other no-return
+// attributes like `_Noreturn` (for C11) and `[[noreturn]]` (for C++ and C23),
+// so we define it here.
 #define NORET __attribute__((__noreturn__))
 
 // Currently, I'm adding these on as-needed basis
