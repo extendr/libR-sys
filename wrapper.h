e42b8aa4ac4e3c17a24af1c137a33224527d32c0
cgmossa <cgmossa@gmail.com>
2023-04-12 22:18:47 +0200

    Remove the legacy solution

--- wrapper.h
+++ wrapper.h
@@ -1,6 +1,3 @@
-// TEMPORARY fix for https://github.com/extendr/extendr/issues/524
-#define R_LEGACY_RCOMPLEX
-
 #include <stddef.h> // for ptrdiff_t
 
 // R_xlen_t is defined as int on 32-bit platforms, and
