2d1a62e47fe3619e7fb46f814ca8bdc3d6d3cfe8
cgmossa <cgmossa@gmail.com>
2023-04-12 22:49:27 +0200

    Added getters/setters

--- wrapper.h
+++ wrapper.h
@@ -47,3 +47,23 @@ typedef ptrdiff_t R_xlen_t_rust;
 Rcomplex create_rcomplex(double real, double imag) {
   return (Rcomplex) { .r = real, .i = imag };
 }
+
+/// Returns the real part
+double Rcomplex_real(const Rcomplex *z) {
+    return z->r;
+}
+
+/// Returns the imaginary part
+double Rcomplex_imaginary(const Rcomplex *z) {
+    return z->i;
+}
+
+/// Set the real part
+void Rcomplex_set_real(Rcomplex *z, double real) {
+    z->r = real;
+}
+
+/// Set the imaginary part
+void Rcomplex_set_imaginary(Rcomplex *z, double imaginary) {
+    z->i = imaginary;
+}
