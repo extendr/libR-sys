cfaff8c57700f0a54f581b23daa81e2e7e2e85fa
CGMossa <cgmossa@gmail.com>
2023-11-13 01:04:32 +0100

    Remove all Fortran items for bindings (#199)
    * Remove all Fortran items
    
    * Update bindings [skip ci]
    
    * Ensures that it can only be one single `_`,
    and not `__` like for some other things in the API
    
    * Update bindings [skip ci]
    
    ---------
    
    Co-authored-by: CGMossa <CGMossa@users.noreply.github.com>

--- bindings/bindings-linux-aarch64-R4.4-devel.rs
+++ bindings/bindings-linux-aarch64-R4.4-devel.rs
@@ -164,8 +164,8 @@ pub const R_MINOR: &[u8; 4] = b"4.0\0";
 pub const R_STATUS: &[u8; 29] = b"Under development (unstable)\0";
 pub const R_YEAR: &[u8; 5] = b"2023\0";
 pub const R_MONTH: &[u8; 3] = b"11\0";
-pub const R_DAY: &[u8; 3] = b"02\0";
-pub const R_SVN_REVISION: u32 = 85465;
+pub const R_DAY: &[u8; 3] = b"11\0";
+pub const R_SVN_REVISION: u32 = 85510;
 pub const R_GE_definitions: u32 = 13;
 pub const R_GE_deviceClip: u32 = 14;
 pub const R_GE_group: u32 = 15;
@@ -5131,34 +5131,6 @@ extern "C" {
         trace: ::std::os::raw::c_int,
         ex: *mut ::std::os::raw::c_void,
     );
-    pub fn dqrqty_(
-        x: *mut f64,
-        n: *mut ::std::os::raw::c_int,
-        k: *mut ::std::os::raw::c_int,
-        qraux: *mut f64,
-        y: *mut f64,
-        ny: *mut ::std::os::raw::c_int,
-        qty: *mut f64,
-    );
-    pub fn dqrqy_(
-        x: *mut f64,
-        n: *mut ::std::os::raw::c_int,
-        k: *mut ::std::os::raw::c_int,
-        qraux: *mut f64,
-        y: *mut f64,
-        ny: *mut ::std::os::raw::c_int,
-        qy: *mut f64,
-    );
-    pub fn dqrcf_(
-        x: *mut f64,
-        n: *mut ::std::os::raw::c_int,
-        k: *mut ::std::os::raw::c_int,
-        qraux: *mut f64,
-        y: *mut f64,
-        ny: *mut ::std::os::raw::c_int,
-        b: *mut f64,
-        info: *mut ::std::os::raw::c_int,
-    );
     #[doc = "appl/pretty.c: for use in engine.c and util.c"]
     pub fn R_pretty(
         lo: *mut f64,
@@ -5199,32 +5171,6 @@ extern "C" {
         a: *mut f64,
         wrk: *mut f64,
         itncnt: *mut ::std::os::raw::c_int,
-    );
-    pub fn dqrdc2_(
-        x: *mut f64,
-        ldx: *mut ::std::os::raw::c_int,
-        n: *mut ::std::os::raw::c_int,
-        p: *mut ::std::os::raw::c_int,
-        tol: *mut f64,
-        rank: *mut ::std::os::raw::c_int,
-        qraux: *mut f64,
-        pivot: *mut ::std::os::raw::c_int,
-        work: *mut f64,
-    );
-    pub fn dqrls_(
-        x: *mut f64,
-        n: *mut ::std::os::raw::c_int,
-        p: *mut ::std::os::raw::c_int,
-        y: *mut f64,
-        ny: *mut ::std::os::raw::c_int,
-        tol: *mut f64,
-        b: *mut f64,
-        rsd: *mut f64,
-        qty: *mut f64,
-        k: *mut ::std::os::raw::c_int,
-        jpvt: *mut ::std::os::raw::c_int,
-        qraux: *mut f64,
-        work: *mut f64,
     );
     pub fn R_sample_kind() -> Sampletype;
     pub fn GetRNGstate();
